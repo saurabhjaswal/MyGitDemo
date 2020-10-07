db8a44c109071e8a646bb75e4c1efb9954bd062d
saurabhworld <s89jaswal@gmail.com>
2020-10-07 08:28:54 +0530

    another code

---
+++ src/main/java/co/qa/test/test12.java
@@ -0,0 +1,10 @@
+package co.qa.test;
+
+public class test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/co/qa/testing/Testing12.java
@@ -0,0 +1,12 @@
+package co.qa.testing;
+
+public class Testing12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hi");
+	}
+
+}
