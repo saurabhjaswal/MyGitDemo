1f7a387f59259b2651d5b125c02b0f0f4fa531ea
saurabhworld <s89jaswal@gmail.com>
2020-10-07 08:54:42 +0530

    added test123 file

--- src/main/java/co/qa/test/test12.java
+++ src/main/java/co/qa/test/test12.java
@@ -5,6 +5,9 @@ public class test12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("hello word this is my first attempt in git");
+		
+		System.out.println("added one mre method");
 	}
 
 }
---
+++ src/main/java/co/qa/test/test23.java
@@ -0,0 +1,12 @@
+package co.qa.test;
+
+public class test23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("adding another class");
+
+	}
+
+}
